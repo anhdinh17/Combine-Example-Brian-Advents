bdb1748244549ef0858052828b80f95b7a6e34c3
Anh Dinh <anhdinh17@gmail.com>
2024-05-14 13:07:51 -0500

    Combine with NotificationCenter

--- Combine Example/ViewController.swift
+++ Combine Example/ViewController.swift
@@ -8,6 +8,11 @@
 import UIKit
 import Combine
 
+struct Message {
+    let content: String
+    let author: String
+}
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var allowMessagesSwitch: UISwitch!
@@ -18,10 +23,14 @@ class ViewController: UIViewController {
     // it's like SwiftUI
     @Published var canSendMessages: Bool = false
     
+    // Create subscriber
     private var switchSubscriber: AnyCancellable?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        messageLabel.numberOfLines = 0
+        allowMessagesSwitch.isOn = false
+        
         setupProcessingChain()
     }
     
@@ -32,21 +41,49 @@ class ViewController: UIViewController {
                 on: DispatchQueue.main
             )
         // When canSendMessage changes value,
-        // subscriber listes to the change, and assign it to
+        // subscriber listens to the change, and assign it to
         // .isEnabled of sendButton
             .assign(
                 to: \.isEnabled,
                 on: sendButon
             )
+        
+        //MARK: - Another way to create publisher and subscriber
+        // This time we use NotificationCenter as example
+        
+        // Create a publisher
+        let messagePublisher = NotificationCenter.Publisher(center: .default, name: .newMessage)
+            .map{ notification -> String? in
+                // Use .map Operator to make sure that the output of publisher (String)
+                // and input of subscriber (String) are the same
+                return (notification.object as? Message)?.content ?? ""
+            }
+        
+        // Create subscriber
+        // We don't use AnyCancellable this time
+        // Like in Switch example, subscriber listens to the change
+        // and assign value to messageLabel.text
+        let messageSubscriber = Subscribers.Assign(object: messageLabel,
+                                                   keyPath: \.text)
+        // Connect publisher and subscriber
+        messagePublisher.subscribe(messageSubscriber)
+        
     }
 
     @IBAction func didSwitch(_ sender: Any) {
         // Value of candSendMessage equals switch .isOn
+        // This is where canSendMessages changes and broadcasts its value
         canSendMessages = (sender as AnyObject).isOn
     }
     
     @IBAction func sendMessage(_ sender: Any) {
-        
+        let message = Message(content: "This is a message at \(Date())", author: "JD")
+        // Publish the value
+        // in this case, we publish the Message object
+        NotificationCenter.default.post(name: .newMessage, object: message)
     }
 }
 
+extension Notification.Name {
+    static let newMessage = Notification.Name("newMessage")
+}
