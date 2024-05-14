925d2a1325f36149575d6678da9714f5efa88dcf
Anh Dinh <anhdinh17@gmail.com>
2024-05-14 11:18:48 -0500

    Simple publisher and subscriber

--- Combine Example/ViewController.swift
+++ Combine Example/ViewController.swift
@@ -6,14 +6,47 @@
 //
 
 import UIKit
+import Combine
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var allowMessagesSwitch: UISwitch!
+    @IBOutlet weak var sendButon: UIButton!
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    // Use @Published to create a publisher
+    // it's like SwiftUI
+    @Published var canSendMessages: Bool = false
+    
+    private var switchSubscriber: AnyCancellable?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupProcessingChain()
+    }
+    
+    func setupProcessingChain() {
+        // Connect subscriber to publisher
+        switchSubscriber = $canSendMessages
+            .receive(
+                on: DispatchQueue.main
+            )
+        // When canSendMessage changes value,
+        // subscriber listes to the change, and assign it to
+        // .isEnabled of sendButton
+            .assign(
+                to: \.isEnabled,
+                on: sendButon
+            )
     }
 
-
+    @IBAction func didSwitch(_ sender: Any) {
+        // Value of candSendMessage equals switch .isOn
+        canSendMessages = (sender as AnyObject).isOn
+    }
+    
+    @IBAction func sendMessage(_ sender: Any) {
+        
+    }
 }
 
